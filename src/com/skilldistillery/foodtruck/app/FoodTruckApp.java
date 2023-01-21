e786d3d847c9a09ee913021740ef147e49cd846a
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-01-21 14:54:05 -0700

    entity and app stubbed out

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,20 @@
 package com.skilldistillery.foodtruck.app;
 
+import com.skilldistillery.foodtruck.entities.Foodtruck;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FoodTruckApp myApp = new FoodTruckApp();
+		Foodtruck[] myTrucks = new Foodtruck[5];
+		
+		myApp.runApp(myTrucks);
 	}
+	
+	private void runApp(Foodtruck[] myTrucks) {
+		
+	}
+	
 
 }
--- src/com/skilldistillery/foodtruck/entities/Foodtruck.java
+++ src/com/skilldistillery/foodtruck/entities/Foodtruck.java
@@ -1,5 +1,79 @@
 package com.skilldistillery.foodtruck.entities;
 
+import java.util.Objects;
+
 public class Foodtruck {
+	private String name;
+	private String foodType;
+	private double rating;
+	private static int nextTruckId = 1;
+	private int id;
+
+	public Foodtruck() {
+	}
+
+	public Foodtruck(String name, String foodType, double rating, int id) {
+		super();
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.id = nextTruckId;
+		nextTruckId++;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(foodType, id, name, rating);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Foodtruck other = (Foodtruck) obj;
+		return Objects.equals(foodType, other.foodType) && id == other.id && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(rating) == Double.doubleToLongBits(other.rating);
+	}
+
+	@Override
+	public String toString() {
+		return "Foodtruck [name=" + name + ", foodType=" + foodType + ", rating=" + rating + ", id=" + id + "]";
+	}
 
 }
